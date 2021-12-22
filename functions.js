1bda37901932ac90948042fb346454bd66449b24
Malike Chapman <ashchapman330@gmail.com>
2021-12-22 16:03:23 -0500

    finished testing capitalize, reversestring, and calculator

--- functions.js
+++ functions.js
@@ -15,8 +15,37 @@ function capitalize(string){
     let newString = firstLetter + string.slice(1);
     return newString;
 }
-function reverseString(string){}
-function calculator(){}
+function reverseString(string){
+    if (string === null)
+    {
+        return null;
+    }
+    if (string.length === 1)
+    {
+        return string;
+    }
+    if (string === "")
+    {
+        return "";
+    }
+    let answer = "";
+    for(let i = string.length - 1; i >= 0; i--)
+    {
+        answer += string.charAt(i);
+    }
+    return answer;
+}
+function calculator(a, b){
+    let object = {
+        add: a + b,
+        subtract: a - b,
+        multiply: a * b,
+        divide: (b === 0 ?  null : a / b)
+            
+        
+    }
+    return object;
+}
 function caesarCipher(){}
 
 module.exports = {capitalize, reverseString, calculator, caesarCipher};
--- functions.test.js
+++ functions.test.js
@@ -19,3 +19,27 @@ test('Null entry is null', () =>{
 test('Empty String', () => {
     expect(capitalize("")).toBe("");
 })
+
+test('Empty reverseString', () => {
+    expect(reverseString("")).toBe("");
+})
+test('null reverseString', () => {
+    const n = null;
+    expect(reverseString(n)).toBeNull();
+})
+test('Length 1 reverseString', () => {
+    expect(reverseString("a")).toBe("a");
+})
+test('regular reverseString', () => {
+    expect(reverseString("test")).toBe("tset");
+})
+test('long reverseString', () => {
+    expect(reverseString("abcdefg")).toBe("gfedcba");
+})
+test('long space reverseString', () => {
+    expect(reverseString("abc defg")).toBe("gfed cba");
+})
+
+test('testing object', () =>{
+    expect(calculator(2, 2)).toMatchObject({add : 4, subtract: 0, multiply: 4, divide: 1});
+})
